d6f9d8bb38c15c693d5cf71b2030669a1072e804
Shahid <malikshahid886@gmail.com>
2022-04-03 15:38:04 +0530

    fedtest

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+ export default function App() {
+    
+        const [name , setName] = useState('');
+        const [age , setAge] = useState('');
+        const [email , setEmail] = useState('');
+        const [password , setPassword] = useState('');
+        const [confPassword , setConfPassword] = useState('');
+     
+        
+        const handleChange =(e)=>{
+          setName(e.target.value);
+        }
+        
+        const handleAgeChange =(e)=>{
+          setAge(e.target.value);
+        }
+        
+        const handleEmailChange =(e)=>{
+          setEmail(e.target.value);
+        }
+          
+        const handlePasswordChange =(e)=>{
+          setPassword(e.target.value);
+        }
+         
+        const handleConfPasswordChange =(e)=>{
+          setConfPassword(e.target.value);
+        }
+        
+        const handleSubmit=(e)=>{
+          if(password!=confPassword)
+          {
+            
+            alert("password Not Match");
+          }
+          else{
+           
+            alert('A form was submitted with Name :"' + name +
+            '" ,Age :"'+age +'" and Email :"' + email + '"');
+          }
+          e.preventDefault();
+     
+        }
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="App-header">
+    <form onSubmit={(e) => {handleSubmit(e)}}>
+    
+   
+    <h3> Sign-up Form </h3>
+    
+        <label >
+          Name:
+        </label><br/>
+        <input type="text" value={name} required onChange={(e) => {handleChange(e)}} /><br/>
+             
+          
+        <label >
+          Age:
+        </label><br/>
+        <input type="text" value={age} required onChange={(e)   => {handleAgeChange(e)}} /><br/>
+            
+          
+        <label>
+          Email:
+        </label><br/>
+        <input type="email" value={email} required onChange={(e) => {handleEmailChange(e)}} /><br/>
+          
+        <label>
+          Password:
+        </label><br/>
+        <input type="password" value={password} required onChange={(e) => {handlePasswordChange(e)}} /><br/>
+              
+              
+        <label>
+          Confirm Password:
+        </label><br/>
+        <input type="password" value={confPassword} required onChange={(e)  => {handleConfPasswordChange(e)}} /><br/>
+             
+                
+        <input type="submit" value="Submit"/>
+      </form>
+    </header>
     </div>
-  );
+    
+  )
 }
-
-export default App;
